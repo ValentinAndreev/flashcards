c566a8a232976a9f61c3d976c4926d667909ecf4
Valentin Andreev <valentinandreev80@gmail.com>
2016-11-10 23:55:05 -0800

    CRUD
    CRUD

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,47 @@
 class CardsController < ApplicationController
+  before_action :set_date
+    
   def index
     @card = Card.all    
   end
+  
+  def show
+    @card = Card.find(params[:id])
+  end
+  
+  def new
+    @card = Card.new
+  end
+  
+  def create
+    @card = Card.new(card_params)
+    if  @card.save    
+      redirect_to cards_path
+    else
+      render 'new'
+    end    
+  end
+  
+  def edit
+    @card = Card.find(params[:id])    
+  end
+  
+  def update
+    @card = Card.find(params[:id])    
+    if @card.update(card_params)  
+      redirect_to cards_path
+    else
+      render 'edit'
+    end      
+  end
+  
+  def destroy
+    @card = Card.find(params[:id])
+    @card.destroy    
+    redirect_to cards_path   
+  end
+  
+  def card_params
+    params.require(:card).permit(:original_text, :translated_text, :review_date)
+  end 
 end
