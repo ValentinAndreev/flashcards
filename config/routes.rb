b36fc8b458957bb42b65478a1ce9bdac7e464f4f
Valentin Andreev <valentinandreev80@gmail.com>
2016-11-15 02:00:32 -0800

    Trainer

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root 'cards#index'  
+  root 'home#showcard'  
   resources :cards
-  
+  post 'answer' => 'trainers#review'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
