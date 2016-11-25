0bd904b5baa1237922637486f7a1e5ddacc111c7
Valentin Andreev <valentinandreev80@gmail.com>
2016-11-24 23:56:40 -0800

    Sorcery

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,12 +1,13 @@
 class TrainersController < ApplicationController
   def review
-    card = Card.find(params[:id])
+    user = User.find(current_user.id)  
+    card = user.cards.find(params[:id])
     translation = CheckTranslation.call(card: card, params: params)
     if translation.success?
-      flash.notice = 'Правильно.'  
+      flash.notice = t(:You_are_right) 
     else
-      flash.notice = 'Не правильно. Правильно: '+ card.translated_text   
+      flash.notice = t(:You_are_wrong_right_translation_is) + card.translated_text
     end
-    redirect_to root_path 
+    redirect_to showcard_path 
   end
 end
