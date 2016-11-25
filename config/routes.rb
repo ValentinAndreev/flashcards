fc1ffb67034d84a13d20b606beb705a8d791fa6c
Valentin Andreev <valentinandreev80@gmail.com>
2016-11-24 23:56:40 -0800

    Sorcery

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,25 @@
 Rails.application.routes.draw do
-  resources :users
-  root 'home#showcard'  
+
+  root 'home#welcome' 
   resources :cards
   post 'answer' => 'trainers#review'
+ 
+  resources :user_sessions
+  resources :users, except: [:new]
   
+  get 'new_user' => 'registrations#new'
+  post 'new_user' => 'registrations#create'  
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
+  
+  get 'showcard' => 'home#showcard', :as => :showcard
+  
+  get 'oauths/oauth'
+  get 'oauths/callback'  
+  post "oauth/callback" => "oauths#callback"
+  get "oauth/callback" => "oauths#callback" 
+  get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
