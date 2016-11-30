99a2d6b0df797203296484b11f0d147a4e09198b
Valentin Andreev <valentinandreev80@gmail.com>
2016-11-29 22:50:21 -0800

    Twitter config

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -4,7 +4,7 @@
 # :reset_password, :session_timeout, :brute_force_protection, :activity_logging, :external
 
 require 'dotenv'
-Dotenv.load(File.expand_path('../twitterkeys.env', __FILE__))
+Dotenv.load(Rails.root.join('config', 'twitterkeys.env'))
 
 Rails.application.config.sorcery.submodules = [:external]
 
