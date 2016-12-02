e10014597f7df4223d5bafc89d08d41460592c61
Valentin Andreev <valentinandreev80@gmail.com>
2016-12-01 21:44:28 -0800

    Add pack

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   end
 
   has_many :authentications, dependent: :destroy
-  has_many :cards, dependent: :destroy
+  has_many :packs, dependent: :destroy
   accepts_nested_attributes_for :authentications
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
@@ -13,8 +13,36 @@ class User < ActiveRecord::Base
 
   validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, uniqueness: { case_sensitive: false }
   
-  private
-  def user_params
-    params.permit(:user).require(:email, :password, :authentications_attributes [:user_id, :provider, :uid])
+  def review_pack
+    packs.find_by(base: true)      
+  end
+
+  def get_card(pack_id)
+    pack = packs.find_by(id: pack_id)
+    pack.cards.randomcard if pack
+  end
+  
+  def set_default(pack)
+    packs.update_all(base: false)
+    pack.update_column(:base, true)
+  end
+
+  def remove_default
+    packs.update_all(base: false)
+  end
+
+  def random_all
+    all_cards = Card.where("user_id = ?", id) 
+    all_cards.order("RANDOM()").first if all_cards
+  end
+
+  def selectcard(pack_id = nil)
+    if (pack_id && review_pack && (pack_id != review_pack.id) && get_card(pack_id)) || (pack_id && !review_pack && get_card(pack_id))
+      [get_card(pack_id) , I18n.t(:Training_with_choosen_pack)]
+    elsif review_pack && get_card(review_pack.id)
+      [get_card(review_pack.id), I18n.t(:Training_with_base_pack)] 
+    else
+      [random_all, I18n.t(:Training_with_random_pack)] if random_all
+    end
   end
 end
