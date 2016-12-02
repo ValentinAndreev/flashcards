c4bfc6a716eb8e43b74b3244bc2c6dd80bd4acf4
Valentin Andreev <valentinandreev80@gmail.com>
2016-12-01 21:44:28 -0800

    Add pack'

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
   def showcard
-    @cardcheck = current_user.cards.randomcard if current_user
-    redirect_to cards_path unless @cardcheck 
+  	if current_user
+      @pack = current_user.packs.find(params[:id]) if params[:id]
+      if @pack
+        @cardcheck = @pack.cards.randomcard
+      else
+        @cardcheck = User.basepack(current_user).cards.randomcard if User.basepack(current_user)
+      end 
+    end
+    redirect_to welcome_path, notice: t(:No_cards_for_training) unless current_user && @cardcheck
   end
 end
