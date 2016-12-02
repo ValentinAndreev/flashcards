99dd7b01cd5146d68d79147fd0f4991941eb3eb1
Valentin Andreev <valentinandreev80@gmail.com>
2016-12-01 21:44:28 -0800

    Add pack

--- spec/features/users/guests_actions_spec.rb
+++ spec/features/users/guests_actions_spec.rb
@@ -7,7 +7,7 @@ end
 
 feature 'Guests actions' do
   before do
-    visit root_path    
+    visit packs_path    
   end
   
   scenario 'guest must see invitation to register or login' do
@@ -18,18 +18,8 @@ feature 'Guests actions' do
     expect(page).to_not have_content t('Edit_profile')    
   end 
   
-  scenario 'guest can`t see cards' do
-    click_on t('All_cards')   
-    expect(page).to_not have_content t('Show')  
+  scenario 'guest can`t see packs and cards' do
+    click_on t('All_packs')      
+    expect(page).to_not have_content t('All_cards')   
   end   
-  
-  scenario 'guest can`t check another users cards' do
-    click_on t('Training')      
-    expect(page).to_not have_content t('Word')  
-  end    
-  
-  scenario 'guest can`t create card' do  
-    click_on t('New_card')    
-    expect(page).to_not have_content t('New')       
-  end 
 end
