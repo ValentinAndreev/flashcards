e79feafe70cc034f27ce163978b4bd93159164ed
Valentin Andreev <valentinandreev80@gmail.com>
2016-12-12 01:08:00 -0800

    New review

--- app/interactors/check_translation.rb
+++ app/interactors/check_translation.rb
@@ -2,11 +2,37 @@ class CheckTranslation
   include Interactor
 
   def call
-    if context.card.translated_text == context.params[:text] then
-       context.card.review_date = 3.days.from_now.to_date
-       context.card.save
+    if context.card.translated_text == context.params[:text]
+      context.card.right_checks += 1 if context.card.right_checks<5
+      context.card.review_date = set_time.hours.from_now if context.card.right_checks != 0 && set_time
+      context.card.save
     else
-       context.fail!
+      if context.card.wrong_checks<2
+        context.card.wrong_checks += 1 if context.card.right_checks>0  
+      else
+        context.card.wrong_checks = 0
+        context.card.right_checks -= 1 if context.card.right_checks>0
+        context.card.review_date = set_time.hours.from_now if context.card.right_checks != 0 && set_time       	
+      end
+      context.card.save      
+      context.fail!
+    end
+  end
+
+  def set_time
+    case context.card.right_checks
+      when 0
+        move_time = 0
+      when 1
+        move_time = 12
+      when 2
+        move_time = 72
+      when 3
+        move_time = 168
+      when 4
+        move_time = 336    
+      when 5
+        move_time = 672                      
     end
   end
 end
