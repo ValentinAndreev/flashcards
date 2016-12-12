6334c64755bce7424e690adedfba47e2fd3d73f3
Valentin Andreev <valentinandreev80@gmail.com>
2016-12-12 01:08:00 -0800

    New review

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -2,7 +2,9 @@ FactoryGirl.define do
   factory :card do
     original_text 'nicht'
     translated_text 'not'
-    review_date Date.today
+    review_date Time.now.strftime('%F %H').to_datetime
+    wrong_checks 0
+    right_checks 0
     association :pack, factory: :pack
     image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'test_files', 'cat.jpg')) }
     image_content_type "image/jpeg"
---
+++ spec/features/cards/check_all_translations_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+require 'support/factory_girl'
+
+def t(string, options={})
+  I18n.t(string, options)
+end
+
+feature 'Check all translations' do 
+  let!(:archived_date) { Time.zone.now }
+
+  before do
+    @user = create(:user)
+    @pack = create(:pack, user: @user)
+    @card = create(:card, pack: @pack, user_id: @user.id)   
+    visit welcome_path     
+    click_on t('Login')
+    fill_in 'Email', with: @user.email
+    fill_in 'Password', with: 'password'
+    click_on t('Log')    
+    expect(page).to have_content @user.email          
+  end
+
+  scenario 'all right translations' do 
+    move_time = [0, 12, 72, 168, 336, 672]
+    i=0
+    6.times {
+      p 'Nowdat:'+Time.now.to_s
+      p 'Review:'+Card.find(@card.id).review_date.to_s
+      Timecop.travel(move_time[i].hours.from_now)
+      p 'Timecp:'+Time.now.to_s
+      visit showcard_path   
+      expect(page).to have_content 'nicht'
+      fill_in t('Translated_text'), with: 'not'
+      click_on t('Send')  
+      expect(page).to have_content t('This_pack_have_no_cards_with_review_date')      
+      Timecop.travel(archived_date)
+      i+=1
+    }
+  end
+end 
