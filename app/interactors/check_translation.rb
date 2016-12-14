9df31ae9ba1599d472f71293ee4499050de71dd7
Valentin Andreev <valentinandreev80@gmail.com>
2016-12-14 14:40:34 +0300

    Levenshtein

--- app/interactors/check_translation.rb
+++ app/interactors/check_translation.rb
@@ -1,18 +1,26 @@
+require 'levenshtein'
+
 class CheckTranslation
   include Interactor
 
   def call
     if context.card.translated_text == context.params[:text]
-      context.card.right_checks += 1 if context.card.right_checks<5
-      context.card.review_date += set_time.hours if context.card.right_checks != 0
+      context.card.right_checks += 1 if context.card.right_checks < 5
+      context.card.review_date += set_time.hours if context.card.right_checks.nonzero?
       context.card.save
     else
-      if context.card.wrong_checks<2
-        context.card.wrong_checks += 1 if context.card.right_checks>0  
+      levens = Levenshtein.distance(context.card.translated_text, context.params[:text])
+      if levens>1
+        context.errors = t(:You_are_wrong_right_translation_is) + context.card.translated_text
+      else
+        context.errors = "#{t(:Wrong_typed_word)}: #{context.params[:text]} #{t(:must_be)}: #{context.card.translated_text}"
+      end
+      if context.card.wrong_checks < 2
+        context.card.wrong_checks += 1 if context.card.right_checks > 0  
       else
         context.card.wrong_checks = 0
-        context.card.right_checks -= 1 if context.card.right_checks>0
-        context.card.review_date -= set_time.hours if context.card.right_checks != 0      	
+        context.card.right_checks -= 1 if context.card.right_checks > 0
+        context.card.review_date -= set_time.hours if context.card.right_checks.nonzero?     	
       end
       context.card.save      
       context.fail!
@@ -22,5 +30,9 @@ class CheckTranslation
   def set_time
     move_time = [0, 12, 72, 168, 336, 672]
     move_time[context.card.right_checks]
+  end
+
+  def t(string, options={})
+    I18n.t(string, options)
   end
 end
