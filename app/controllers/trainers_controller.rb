38daa337a68e730b0b61f2519afba041b493a6b5
Valentin Andreev <valentinandreev80@gmail.com>
2016-12-14 14:40:34 +0300

    Levenstein

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,3 +1,5 @@
+require '/home/z/.rbenv/versions/2.3.1/lib/ruby/gems/2.3.0/gems/levenshtein-c-0.1.0/lib/levenshtein.so'
+
 class TrainersController < ApplicationController
   def review
     card = Card.find(params[:id])
@@ -5,8 +7,13 @@ class TrainersController < ApplicationController
     if translation.success?
       flash.notice = t(:You_are_right) 
     else
-      flash.notice = t(:You_are_wrong_right_translation_is) + card.translated_text
+      levens = Levenshtein.distance(card.translated_text, params[:text])
+      if levens>1
+        flash.notice = t(:You_are_wrong_right_translation_is)+card.translated_text
+      else
+        flash.notice = "#{t(:Wrong_typed_word)}: #{params[:text]} #{t(:must_be)}: #{card.translated_text}"
+      end
     end
-    redirect_to :back
+    redirect_to showcard_path
   end
 end
