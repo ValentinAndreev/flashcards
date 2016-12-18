72246f645e1c05e33776ec6d8689d811c393ce45
Valentin Andreev <valentinandreev80@gmail.com>
2016-12-18 15:35:09 +0300

    Mail

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,16 @@
+require 'dotenv'
+
+class ApplicationMailer < ActionMailer::Base
+
+Dotenv.load(Rails.root.join('config', 'email.env'))	
+ActionMailer::Base.smtp_settings = {
+  user_name: ENV['MAIL_NAME'],
+  password: ENV['MAIL_PASSWORD'],
+  domain: 'mail.ru',
+  address: 'smtp.mail.ru',
+  port: 465,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
+  layout 'mailer'
+end
