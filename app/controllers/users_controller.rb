501e700606e3bcf86a13179e89dddb8d5c16db71
Valentin Andreev <valentinandreev80@gmail.com>
2016-12-22 22:19:58 +0300

    I18n

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :locale)
   end
 end
