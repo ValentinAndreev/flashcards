3a39a7852c8407c69dbd5bad622d809e85b71b33
Valentin Andreev <valentinandreev80@gmail.com>
2016-12-26 14:54:55 +0300

    Supermemo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@
 class HomeController < ApplicationController
   def showcard
     @cardcheck = current_user.select_card
+    p @cardcheck
     redirect_to packs_path, notice: t(:No_cards_for_training) unless @cardcheck[0]
   end
 
